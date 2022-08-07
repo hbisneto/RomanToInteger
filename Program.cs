3b5174ed00d4be5a157ee22babeea6d730948199
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2022-08-07 17:45:36 -0300

    RomanToInteger

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RomanToInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(">> Escreva um número (Romano): ");
+            string RomanNumber = Console.ReadLine().ToUpper();
+
+            var Core = new RomanCore();
+            Console.WriteLine(">> Algarismos Arábicos: " + Core.RomanToInteger(RomanNumber));
+
+            Console.WriteLine("\n\n>> Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RomanCore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace RomanToInteger
+{
+    internal class RomanCore
+    {
+        public int RomanToInteger(string RomanString)
+        {
+            int Soma = 0;
+            Dictionary<char, int> RomanDictionary = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            for (int i = 0; i < RomanString.Length; i++)
+            {
+                char RomanChar = RomanString[i];
+                RomanDictionary.TryGetValue(RomanChar, out int Numero);
+                if (i + 1 < RomanString.Length && RomanDictionary[RomanString[i + 1]] > RomanDictionary[RomanChar])
+                {
+                    Soma -= Numero;
+                }
+                else
+                {
+                    Soma += Numero;
+                }
+            }
+            return Soma;
+        }
+    }
+}
